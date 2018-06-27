744b8028ceccb56c0809ef673784bedf9cfdcc61
kgkbuh2 <buh2@kgk-global.com>
2018-06-27 16:41:18 +0300

    Test

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -13,6 +13,8 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
 
+    private boolean mRequiresPolice;
+
     public Crime() {
         mId = UUID.randomUUID();
         mDate = new Date();
@@ -44,5 +46,13 @@ public class Crime {
 
     public void setSolved(boolean mSolved) {
         this.mSolved = mSolved;
+    }
+
+    public boolean isRequiresPolice() {
+        return mRequiresPolice;
+    }
+
+    public void setRequiresPolice(boolean requiresPolice) {
+        mRequiresPolice = requiresPolice;
     }
 }
